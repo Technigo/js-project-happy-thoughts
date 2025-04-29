047ca9a0554c735e86692a6ab44415d588ab4a9e
Johan Skarpsvärd <johanconsult@protonmail.com>
2025-04-29 17:33:43 +0200

    Code-lab-web

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import {useState} from "react";
+import "./index.css";
+// character limit exceeded when char >140
+export default function App () {
+    const [text ,setText] - useState("");
+    const[error, setError] = useState("");
+    const handle Change -(e) -> {
+        console log (e.target.value);
+        if(e.target.value.length >140
+        {
+            
+           // set text(e.target.value) 
+            setError("character limit exceeded")
+            else{
+                setError(null)
+            }
+        }
+        return(
+            <div className-"App">
+            <div className - "input-container">
+                <input className - "input" 
+                type - "text"
+                placeholder - " enter something ..."
+                on Change-{handleChange}
+                value-{text}
+                />
+                <span className-"error">{error && error}</span>
+                
+
+                </ div>
+                </ div>
+        );
+    )
+        )
+    }
+}
+
+}
