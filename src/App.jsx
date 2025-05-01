17ff0a7faf9f18b4150925989d7f6a9982a7b791
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-05-01 11:56:09 +0200

    added files and basic html

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-export const App = () => {
+import { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [messages, setMessages] = useState([]);
+
+  const addMessage = (newMessage) => {
+    setMessages([newMessage, ...messages]);
+  };
+
   return (
-    <h1>Happy Thoughts</h1>
-  )
-}
+    <>
+      <h1>Happy Thoughts</h1>
+      <div className="">
+        <p>Is there anything that makes you happy right now?</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
