e2f8fb29af1865072122b337782a13c46f02e3ca
MalLunBar <malinelundgren1991@gmail.com>
2025-05-09 11:15:57 +0200

    showcase

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Loader } from "./components/Loader"
 import { MainSection } from "./section/MainSection"
 
 
@@ -9,8 +10,9 @@ export const App = () => {
 
   return (
     <>
+
       <MainSection />
-      
+
     </>
   )
 }
