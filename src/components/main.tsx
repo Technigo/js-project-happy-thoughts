580ea4eed81b1bc0ec7b072b01a33a96638a2911
Johan Skarpsvärd <johanconsult@protonmail.com>
2025-05-09 14:27:05 +0200

    Code-lab-web

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -2,6 +2,12 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from '../App.tsx'
+import Card from './Card.tsx';
+import './Card.css';
+// Removed conflicting import of App
+import { Form } from './form';
+import Main from './main.tsx';
+import indexData from './index.json';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
