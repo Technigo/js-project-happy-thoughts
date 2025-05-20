31ddc43a6a2865941ad7edd227e5b803c5e970cd
Bianka R <bianka_romero@me.com>
2025-05-21 01:48:15 +0200

    title banner animation

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,18 +1,31 @@
+import { keyframes } from "styled-components"
 import styled from "styled-components"
 
+const scrollingTitle = keyframes`
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+`
+const TitleContainer = styled.div`
+  overflow: hidden;
+  white-space: nowrap;
+`
 const Title = styled.h1`
   margin: 0 auto;
   width: 100%;
   text-align: center;
   padding: 2rem 1rem;
-
+  animation: ${scrollingTitle} 10s linear infinite;
 `
 
 const Header = () => {
   return (
-    <> 
-    <Title>Happy Thoughts</Title>
-    </>
+    <TitleContainer> 
+    <Title> 💬 Happy Thoughts 💬 </Title>
+    </TitleContainer>
   )
 }
 
--- src/sections/MsgBoard.jsx
+++ src/sections/MsgBoard.jsx
@@ -11,19 +11,18 @@ export const MsgBoard = () => {
 
   const url = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts"
 
-  // 1. Fetch Data
   const fetchAPI = async () => {
     setLoading(true)
     try {
       const response = await fetch(url)
       if (response.ok) {
         const data = await response.json()
-        // set the state
-        console.log(data)
         setThoughts(data)
       }
+
     } catch(error) {
       console.log(error)
+
     } finally {
       setLoading(false)
     }
