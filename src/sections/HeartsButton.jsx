d2be90c1bf25d38d665560ede5289466e8f6f5ac
Bianka R <bianka_romero@me.com>
2025-05-23 01:16:57 +0200

    add like fx! 🥳

--- src/sections/HeartsButton.jsx
+++ src/sections/HeartsButton.jsx
@@ -18,13 +18,33 @@ const CountText = styled.p`
   color: #a2a3a4;
 `
 
-const HeartsButton = ({ hearts }) => {
+const HeartsButton = ({ hearts, id }) => {
   const [count, setCount] = useState(hearts)
+
+  const handleLike = async (event) => {
+    event.preventDefault()    
+    
+    try {
+      const response = await fetch(`https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${id}/like`, {
+        method: "POST",
+      })
+
+      if (!response.ok) {
+        throw new Error("Failed to post like")
+      }
+
+      const newLike = await response.json()
+        setCount(newLike.hearts)
+        
+    } catch (error) {
+      console.error("Error liking message:", error)
+    }
+  }
 
   return (
     <ButtonContainer> 
         <LikeButton
-          onClick={() => setCount((prev) => prev+1)}>♥️
+          onClick={handleLike}>♥️
         </LikeButton>
         <CountText>x {count}</CountText>
       </ButtonContainer>
