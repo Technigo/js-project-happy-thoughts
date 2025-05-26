ef616b4cab5b7fb46f3a5a99c799c4a637c6abec
Jonny Hicks <hello.jonnyhicks@gmail.com>
2025-05-26 16:01:42 +0200

    Update API endpoint

--- src/Components/HandlesLikes.jsx
+++ src/Components/HandlesLikes.jsx
@@ -1,7 +1,7 @@
 const handleLike = async (id) => {
   try {
     const response = await fetch(
-      `https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${id}/like`,
+      `https://happy-thoughts-api-4ful.onrender.com/thoughts/${id}/like`,
       {
         method: "POST",
       }
