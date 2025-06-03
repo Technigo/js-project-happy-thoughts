d854299793a67cc69da03d7fd04f9747a043d915
Johan Skarpsvärd <johanconsult@protonmail.com>
2025-06-03 15:04:41 +0200

    git commit -m "Code-lab-web"

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,19 @@
+<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot} from 'react-dom/client'
+=======
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './components/App.js'
+import { App } from './components/App.jsx'
+import './components/index.css'
+import Form from './components/form.jsx'
+import Card from './components/Card.tsx'
+import './components/Card.css'
+import './components/App.tsx'
+import './components/animation.jsx'
+import './components/animation.css'
+>>>>>>> d4f14ca (git commit -m "Code-lab-web")
 
 import { App } from './App.jsx'
 
