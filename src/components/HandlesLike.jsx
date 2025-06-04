2c86f89bd915de18c475c110c757b148874b5486
Jonny Hicks <hello.jonnyhicks@gmail.com>
2025-06-04 20:03:54 +0200

    Fix duplicate slash in fetch URL for liking thoughts

--- src/components/HandlesLike.jsx
+++ src/components/HandlesLike.jsx
@@ -1,11 +1,10 @@
 const handleLike = async (id) => {
   try {
     const response = await fetch(
-      `https://js-project-api-k17p.onrender.com//thoughts/${id}/like`,
-      {
-        method: "POST",
-      }
+      `https://js-project-api-k17p.onrender.com/thoughts/${id}/like`,
+      { method: "POST" }
     );
+
     if (response.ok) {
       return await response.json();
     } else {
